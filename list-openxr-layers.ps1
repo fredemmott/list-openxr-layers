10e1f88b172e69705ad4481f1157aca5ed32d738
Fred Emmott <git@fredemmott.com>
2023-02-12 13:17:30 -0600

    Add 32-bit support

--- list-openxr-layers.ps1
+++ list-openxr-layers.ps1
@@ -1,38 +1,43 @@
 # Copyright (c) 2023 Fred Emmott <fred@fredemmott.com>
 #
 # SPDX-License-Identifier: MIT
-$enabledCount = 0
-foreach ($root in @("HKLM", "HKCU")) {
-	$key = "${root}:\SOFTWARE\Khronos\OpenXR\1\ApiLayers\Implicit"
-	Write-Host "${root}"
-	if (-not (Test-Path $key)) {
-		Write-Host -Foreground DarkGray "`tNo layers."
-		continue;
-	}
-
-	$layers = (Get-Item $key).Property
-	if ($layers.count -eq 0) {
-		Write-Host -Foreground DarkGray "`tNo layers."
-		continue;
-	}
-
-	foreach ($layer in $layers) {
-		if (-not (Test-Path "${layer}")) {
-			Write-Host -ForegroundColor Red "`t${layer} does not exist (${root})"
+#
+function dump($label, $subkey) {
+	$enabledCount = 0
+	foreach ($root in @("HKLM", "HKCU")) {
+		$key = "${root}:\${subkey}"
+		Write-Host "${label} ${root}"
+		if (-not (Test-Path $key)) {
+			Write-Host -Foreground DarkGray "`tNo layers."
 			continue;
 		}
 
-		$name = (Get-Content "${layer}" | ConvertFrom-Json).api_layer.name
-		$disabled = Get-ItemPropertyValue $key -name $layer
-		if ($disabled) {
-			Write-Host -ForegroundColor DarkRed "`t${name}"
+		$layers = (Get-Item $key).Property
+		if ($layers.count -eq 0) {
+			Write-Host -Foreground DarkGray "`tNo layers."
+			continue;
 		}
-		else {
-			$enabledCount++
-			Write-Host -NoNewline "`t (#${enabledCount})"
-			Write-Host -NoNewline -ForegroundColor DarkGray " - "
-			Write-Host -ForegroundColor Green "${name}"
+
+		foreach ($layer in $layers) {
+			if (-not (Test-Path "${layer}")) {
+				Write-Host -ForegroundColor Red "`t${layer} does not exist (${root})"
+				continue;
+			}
+
+			$name = (Get-Content "${layer}" | ConvertFrom-Json).api_layer.name
+			$disabled = Get-ItemPropertyValue $key -name $layer
+			if ($disabled) {
+				Write-Host -ForegroundColor DarkRed "`t${name}"
+			}
+			else {
+				$enabledCount++
+				Write-Host -NoNewline "`t (#${enabledCount})"
+				Write-Host -NoNewline -ForegroundColor DarkGray " - "
+				Write-Host -ForegroundColor Green "${name}"
+			}
+			Write-Host -ForegroundColor DarkGray "`t`t${layer}"
 		}
-		Write-Host -ForegroundColor DarkGray "`t`t${layer}"
 	}
 }
+dump '64-bit' "SOFTWARE\Khronos\OpenXR\1\ApiLayers\Implicit"
+dump '32-bit' "SOFTWARE\WOW6432Node\Khronos\OpenXR\1\ApiLayers\Implicit"
